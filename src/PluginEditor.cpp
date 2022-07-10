8ae66f486a5e2dbd8be427fc681688a298a6a3c6
rodoortiz <rodolfortiz94@gmail.com>
2022-07-09 23:43:15 -0500

    background

--- src/PluginEditor.cpp
+++ src/PluginEditor.cpp
@@ -3,10 +3,7 @@
 
 ModelizerAudioProcessorEditor::ModelizerAudioProcessorEditor (ModelizerAudioProcessor& p) : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    addAndMakeVisible (button);
-    button.addListener (this);
-
-    setSize (600, 300);
+    setSize (500, 300);
 }
 
 ModelizerAudioProcessorEditor::~ModelizerAudioProcessorEditor() {}
@@ -14,6 +11,26 @@ ModelizerAudioProcessorEditor::~ModelizerAudioProcessorEditor() {}
 void ModelizerAudioProcessorEditor::paint (juce::Graphics& g)
 {
     g.fillAll(juce::Colours::white);
+
+    juce::Rectangle<int> headerRect {getWidth(), getHeight() / 6};
+    g.setColour (juce::Colour (0XFFFC7558));
+    g.fillRect (headerRect);
+
+    juce::Rectangle<int> textRect { 200, 20 };
+    textRect.setCentre(250, 24);
+    g.setFont (24.0f);
+    g.setColour (juce::Colours::white);
+    g.drawText ("MODULIZER", textRect, juce::Justification::centred);
+
+    g.setColour(juce::Colour (0XFFDFDFDF));
+    juce::Rectangle<float> borderRect {130.0f, 230.0f};
+    borderRect.setCentre(80,175);
+    g.drawRoundedRectangle(borderRect, 10.0f, 2.0f);
+
+    g.setColour(juce::Colour (0XFFDFDFDF));
+    juce::Rectangle<float> borderRect2 { 310.0f, 180.0f };
+    borderRect2.setCentre(325,200);
+    g.drawRoundedRectangle(borderRect2, 10.0f, 2.0f);
 }
 
 void ModelizerAudioProcessorEditor::resized()
